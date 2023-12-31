faad45a8317d8adbc51da300923247c94bb67893
ashonasho <ashonmcmillan1234@gmail.com>
2023-12-31 13:15:31 +0530

    your name bug fixed

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import datetime
 st.title("Let's Date")
 st.header("In the world of our Dating App, possibilities are endless. Discover the chemistry, embrace the excitement, and let your perfect date unfold in style.")
 
-yourname = st.text_input("Yourname", placeholder="short name/your name")
-st.write("welcome", yourname)
+name = st.text_input("Yourname", placeholder="short name/your name")
+st.write("welcome", name)
 
 firstname = st.text_input("Firstname", placeholder="Enter your first name")
 secondname = st.text_input("Secondname", placeholder="Enter your second name")
