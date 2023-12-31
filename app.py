5cf2d40266cb01b5c613dbbbd4ae0ed7008d76f9
ashonasho <ashonmcmillan1234@gmail.com>
2023-12-31 12:54:27 +0530

    added place holder in greeting session

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("Let's Date")
 st.header("In the world of our Dating App, possibilities are endless. Discover the chemistry, embrace the excitement, and let your perfect date unfold in style.")
 
-name = st.text_input("")
+name = st.text_input("", placeholder="short name/your name")
 st.write("welcome", name)
 firstname = st.text_input("Firstname", placeholder="Enter your first name")
 secondname = st.text_input("Secondname", placeholder="Enter your second name")
