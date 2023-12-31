f85e9d096174f57d9a00460405755bf9f59748a4
ashonasho <ashonmcmillan1234@gmail.com>
2023-12-31 12:23:23 +0530

    title added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st 
 
-
+st.title("Let's Date")
 st.header("In the world of our Dating App, possibilities are endless. Discover the chemistry, embrace the excitement, and let your perfect date unfold in style.")
