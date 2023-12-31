3f8c90dc2fa781e85131700c817ef5b30b97a7f8
ashonasho <ashonmcmillan1234@gmail.com>
2023-12-31 13:18:17 +0530

    placeholder added in birth date

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ st.title("Let's Date")
 st.header("In the world of our Dating App, possibilities are endless. Discover the chemistry, embrace the excitement, and let your perfect date unfold in style.")
 
 name = st.text_input("Yourname", placeholder="short name/your name")
-st.write("welcome", name)
+st.write("Welcome", name)
 
 firstname = st.text_input("Firstname", placeholder="Enter your first name")
 secondname = st.text_input("Secondname", placeholder="Enter your second name")
-birthdate= st.date_input("Birth Date", format="DD.MM.YYYY")
+birthdate= st.date_input("Birth Date", format="DD.MM.YYYY", placeholder="DD.MM.YYYY")
