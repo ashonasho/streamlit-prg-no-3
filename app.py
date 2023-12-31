7af2aa90efade83ad5aec49ee2e189d0f439e819
ashonasho <ashonmcmillan1234@gmail.com>
2023-12-31 12:59:14 +0530

    name changed to your name

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("Let's Date")
 st.header("In the world of our Dating App, possibilities are endless. Discover the chemistry, embrace the excitement, and let your perfect date unfold in style.")
 
-name = st.text_input("", placeholder="short name/your name")
-st.write("welcome", name)
+yourname = st.text_input("Yourname", placeholder="short name/your name")
+st.write("welcome", yourname)
 firstname = st.text_input("Firstname", placeholder="Enter your first name")
 secondname = st.text_input("Secondname", placeholder="Enter your second name")
