bcec0cfea1deefeface12609dab32a3ec09c6d6c
ashonasho <ashonmcmillan1234@gmail.com>
2023-12-31 13:09:49 +0530

    bug in welcone your name fixed

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import datetime
 
 st.title("Let's Date")
 st.header("In the world of our Dating App, possibilities are endless. Discover the chemistry, embrace the excitement, and let your perfect date unfold in style.")
-st.write("welcome", yourname)
 
 
 yourname = st.text_input("Yourname", placeholder="short name/your name")
 firstname = st.text_input("Firstname", placeholder="Enter your first name")
 secondname = st.text_input("Secondname", placeholder="Enter your second name")
+st.write("welcome", yourname)
 birthdate= st.date_input("Birth Date", format="DD.MM.YYYY")
