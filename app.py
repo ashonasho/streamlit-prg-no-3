519a847b6211b2c71d3da92cdcf4dc46cbd7ade1
ashonasho <ashonmcmillan1234@gmail.com>
2023-12-31 13:07:50 +0530

    birth date input

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st 
+import datetime
 
 st.title("Let's Date")
 st.header("In the world of our Dating App, possibilities are endless. Discover the chemistry, embrace the excitement, and let your perfect date unfold in style.")
+st.write("welcome", yourname)
+
 
 yourname = st.text_input("Yourname", placeholder="short name/your name")
-st.write("welcome", yourname)
 firstname = st.text_input("Firstname", placeholder="Enter your first name")
 secondname = st.text_input("Secondname", placeholder="Enter your second name")
+birthdate= st.date_input("Birth Date", format="DD.MM.YYYY")
