e7438c6e88566c700687e09f0c25d357dd0c69cb
ashonasho <ashonmcmillan1234@gmail.com>
2023-12-31 12:41:13 +0530

    same name bug check

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title("Let's Date")
 st.header("In the world of our Dating App, possibilities are endless. Discover the chemistry, embrace the excitement, and let your perfect date unfold in style.")
 
-Name = st.text_input("")
-st.write("welcome", Name)
+name = st.text_input("")
+st.write("welcome", name)
