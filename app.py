780ca99b69a5a99a5e16659768a0a1c6ad6a8f0a
ashonasho <ashonmcmillan1234@gmail.com>
2023-12-31 12:16:31 +0530

    welcome bug cleared

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st 
 
 
-st.write("In the world of our Dating App, possibilities are endless.
-          Discover the chemistry, embrace the excitement, and let your perfect date unfold in style.")
+st.write("In the world of our Dating App, possibilities are endless. Discover the chemistry, embrace the excitement, and let your perfect date unfold in style.")
