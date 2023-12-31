4c532d9db9e37174c2929407512841eca73b9308
ashonasho <ashonmcmillan1234@gmail.com>
2023-12-31 13:13:22 +0530

    error in welocome your name fixed

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st 
 import datetime
 
+
 st.title("Let's Date")
 st.header("In the world of our Dating App, possibilities are endless. Discover the chemistry, embrace the excitement, and let your perfect date unfold in style.")
 
-
 yourname = st.text_input("Yourname", placeholder="short name/your name")
+st.write("welcome", yourname)
+
 firstname = st.text_input("Firstname", placeholder="Enter your first name")
 secondname = st.text_input("Secondname", placeholder="Enter your second name")
-st.write("welcome", yourname)
 birthdate= st.date_input("Birth Date", format="DD.MM.YYYY")
