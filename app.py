920ee548532b883f3d6da8f9f72d2c066a57991c
ashonasho <ashonmcmillan1234@gmail.com>
2024-01-09 15:51:48 +0530

    button variable error

--- app.py
+++ app.py
@@ -88,6 +88,7 @@ def display_matches(matches):
     #     st.write("No matching profiles found.")
 
 def main():
+    button=None
     check = 0
     st.title("Let's Date")
     st.header("In the world of our Dating App, possibilities are endless. Discover the chemistry, embrace the excitement, and let your perfect date unfold in style.")
