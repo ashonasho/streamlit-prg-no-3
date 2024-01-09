37dbee025d4a4d33acc0af916821596beb2584a1
ashonasho <ashonmcmillan1234@gmail.com>
2024-01-09 16:31:55 +0530

    change in prompt

--- app.py
+++ app.py
@@ -223,7 +223,7 @@ def main():
                 user_prompt = f"here is my datas {details}.and prioritize matches who are {date_gender}. and FOLLOW {date_religion}.give only highly recomended data. dont give me the sample datas"
 
             elif high_preference == "gender & job":
-                user_prompt =  f"here is my datas {details}.and prioritize matches who are {date_gender} and has job as the {date_job}.give the most suitable profiles. dont give me the sample datas"
+                user_prompt =  f"here is my datas {details}.and prioritize matches who are {date_gender} and has job as the {date_job}.give the most suitable profile. dont give me the sample datas"
 
             elif high_preference == "All":
                 user_prompt =  f"here is my datas {details}.and prioritize matches who are {date_gender} and FOLLOW {date_religion}.and has job as the {date_job} give in ordered list view anddont give me the sample datas"
